5ea84d8f522d347e85aab91254f78b0440a1a814
Group1_Act2_Collab <Group1@gmail.com>
2021-11-07 09:16:03 +0000

    Group1 Original program

---
+++ sci_formula.py
@@ -0,0 +1,45 @@
+print("============================================")
+print("||         Area & Volume Calculator       ||")
+print("============================================")
+print("\n[A] Cylinder [B] Cube [C] Prism")
+shape = str(input("Choose shape to measure: "))
+if shape == "A":
+    print("\n[1] Area [2] Volume")
+    formula = int(input("Choose parameter to compute: "))
+    if formula == 1:
+        radius = float(input("\nPlease key in radius (m): "))
+        heightc = float(input("Please key in height (m): "))
+        areacy = (2 * 3.1416 * radius * heightc) + (2 * 3.1416 * radius * radius)
+        print("\nThe area of cylinder is {} sq.m.".format(areacy))
+    if formula == 2:
+        radius = float(input("\nPlease key in radius (m): "))
+        height = float(input("Please key in height (m): "))
+        volumecy = 3.1416 * radius * radius * height
+        print("\nThe volume of cylinder is {} cu.m.".format(volumecy))
+if shape == "B":
+    print("\n[1] Area [2] Volume")
+    formula = int(input("Choose parameter to compute: "))
+    if formula == 1:
+        side = float(input("\nPlease key in side (m): "))
+        areacb = 6 * side * side
+        print("\nThe area of cube is {} sq.m.".format(areacb))
+    if formula == 2:
+        side = float(input("\nPlease key in side (m): "))
+        volumecb = side * side * side
+        print("\nThe volume of cube is {} cu.m.".format(volumecb))
+if shape == "C":
+    print("\n[1] Area [2] Volume")
+    formula = int(input("Choose parameter to compute: "))
+    if formula == 1:
+        base = float(input("\nPlease key in base (m): "))
+        heightp = float(input("Please key in height (m): "))
+        lengthp = float(input("Please key in length (m): "))
+        sidep = float(input("Please key in side (m): "))
+        areap = base * heightp + 2*lengthp*sidep
+        print("\nThe area of prism is {} sq.m.".format(areap))
+    if formula == 2:
+        lengthp = float(input("\nPlease key in length (m): "))
+        width = float(input("Please key in width (m): "))
+        heightp = float(input("Please key in height (m): "))
+        volumep = lengthp * width * heightp
+        print("\nThe volume of cube is {} cu.m.".format(volumep))
